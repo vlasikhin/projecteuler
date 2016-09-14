b81d2a0a4a0b1b1d3d52c472db4b25db50cab5e9
Pavel Vlasikhin <vlasikhinpavel@gmail.com>
2016-09-14 10:29:41 +0300

    refactor app1

--- app1.rb
+++ app1.rb
@@ -1,4 +1,4 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
@@ -6,10 +6,7 @@
 sum = 0
 
 1.upto(1000) do |x|
-
-	if x % 3 == 0 || x % 5 == 0
-		sum += x
-  end	
-
+  sum += x if (x % 3).zero? || (x % 5).zero?
 end
+
 puts sum
